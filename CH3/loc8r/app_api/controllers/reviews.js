26ed9d56a90525d88db477d7ec0e51ed5204f210
Nicolás Rafael Riquelme Guzmán <nrriquelme@gmail.com>
2017-01-02 22:48:04 -0300

    Fin de la API REST con todos los metodos a utilizar.

--- CH3/loc8r/app_api/controllers/reviews.js
+++ CH3/loc8r/app_api/controllers/reviews.js
@@ -6,6 +6,29 @@ var sendJsonResponse = function(res, status, content) {
   res.json(content);
 };
 
+var doAddReview = function(req, res, location){
+  if(!location){
+    sendJsonResponse(res, 404, {
+      "message": "locationid not found"
+    });
+  } else {
+    location.reviews.push({
+      author: req.body.author,
+      rating: req.body.rating,
+      reviewText: req.body.reviewText
+    });
+    location.save(function(err, location){
+      var thisReview;
+      if(err){
+        sendJsonResponse(res, 400, err);
+      } else{
+        thisReview = location.reviews[location.reviews.length-1];
+        sendJsonResponse(res, 201, thisReview);
+      }
+    });
+  }
+};
+
 module.exports.reviewsReadOne = function(req, res){
   console.log('Getting single review');
   if(req.params && req.params.locationid && req.params.reviewid){
